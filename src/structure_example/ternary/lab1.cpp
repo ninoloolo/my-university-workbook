6d18c394c02b59731ecdcd7b6ce9073809f24d4b
ninoloolo <117465307+ninoloolo@users.noreply.github.com>
2023-03-08 22:59:26 +0000

    смена формата

--- src/structure_example/ternary/lab1.cpp
+++ src/structure_example/ternary/lab1.cpp
@@ -1,14 +1,10 @@
 #include <cstdio>
-#include <iostream>
 
-int main(){
-    int num;
+int main()
+{
+	int num;
+	printf("Введіть число = ");
+	scanf("%d", &num);
 
-    printf("Введіть число = %d");
-    scanf("%d", &num);
-
-    char fiveteen = (num % 15 == 0) ? printf("Число кратне 3 та 5.") : printf("Не кратне 3 та 5");
-    char three = (num % 3 == 0) ? printf("Число кратне 3.") : printf("Не кратне 5");
-    char five = (num / 5) ? printf("Число кратне 5.") : printf("Не кратне 3");
-
+    char x = ((num % 15 == 0)) ? printf("Число кратне 3 и 5. \n") : (num % 3 == 0) ? printf("Число кратне лише 3. \n") : printf("Число кратне лише 5. \n");
 }
