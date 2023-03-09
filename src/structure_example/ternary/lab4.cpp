e4198d257b2f559cdb23cd2339b2e075b7919f7b
ninoloolo <117465307+ninoloolo@users.noreply.github.com>
2023-03-09 12:06:41 +0000

    calculator

---
+++ src/structure_example/ternary/lab4.cpp
@@ -0,0 +1,43 @@
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+    char oper;
+    float num1, num2;
+
+    printf("Введіть операцію (+, -, *, /): ");
+    scanf(" %c", &oper);
+
+    printf("Введіть два числа: ");
+    scanf("%f %f", &num1, &num2);
+
+    switch (oper) {
+        case '+':
+            printf("%.2f + %.2f = %.2f", num1, num2, num1 + num2);
+        break;
+
+        case '-':
+            printf("%.2f - %.2f = %.2f", num1, num2, num1 - num2);
+        break;
+
+        case '*':
+            printf("%.2f * %.2f = %.2f", num1, num2, num1 * num2);
+        break;
+
+        case '/':
+            if (num2 == 0) {
+                printf("Неможливо поділити на нуль!");
+            }
+            else {
+                printf("%.2f / %.2f = %.2f", num1, num2, num1 / num2);
+            }
+        break;
+
+        default:
+            printf("Невірний знак операції!");
+        break;
+    }
+
+    return 0;
+}
